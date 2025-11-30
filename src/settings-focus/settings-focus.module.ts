9e96e6c017d0f9e2b8b84bbdb920d2a89b853ef5
novvais <eduardodepaulanovais10@gmail.com>
2025-11-30 16:40:30 -0300

    feat: adiciona theme_color nas configurações de foco

--- src/settings-focus/settings-focus.module.ts
+++ src/settings-focus/settings-focus.module.ts
@@ -3,9 +3,10 @@ import { SettingsFocusController } from './settings-focus.controller';
 import { SettingsFocusService } from './settings-focus.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { LogsModule } from 'src/logs/logs.module';
+import { PlanModule } from 'src/plan/plan.module';
 
 @Module({
-  imports: [PrismaModule, LogsModule],
+  imports: [PrismaModule, LogsModule, PlanModule],
   controllers: [SettingsFocusController],
   providers: [SettingsFocusService],
   exports: [SettingsFocusService],
